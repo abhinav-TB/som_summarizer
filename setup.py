fdcd45a66dde60d5e4ad2df689dcefa64c21770a
Abhinav <abhinavtb@gmail.com>
2022-04-07 09:43:10 +0000

    new release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="som-summarizer",
-    version="1.0.6",
+    version="1.0.7",
     description="Summarize long ducuments of all types",
     long_description=README,
     long_description_content_type="text/markdown",
