ba70ce192e0a3d9b8cdd0824d1a0a7c3acf70a1e
Abhinav <abhinavtb@gmail.com>
2022-04-11 14:16:27 +0000

    added caching

--- som_summarizer/som.py
+++ som_summarizer/som.py
@@ -1,5 +1,11 @@
 from sklearn_som.som import SOM
+import streamlit as st
 from sentence_transformers import SentenceTransformer
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+
 class som:
     def __init__(self,m,n,epochs,text_tokens):
         self.som_obj = SOM(m, n, dim=768)
@@ -10,6 +16,7 @@ class som:
         sentence_embeddings = self.generate_embeddings()
         self.som_obj.fit(sentence_embeddings , self.epochs)
         return self.som_obj.predict(sentence_embeddings)
+
     def generate_embeddings(self):
-        sbert_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+        sbert_model = load_model()
         return sbert_model.encode(self.text_tokens)
